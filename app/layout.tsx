0e8887fc3c0fe049c58b2819490bf963184abf97
SaherAkhtar <your-github-saherakhtar2004@gmail.com>
2026-07-31 04:29:48 -0700

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import WhatsAppButton from "@/components/WhatsAppButton";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
